10d47ad7bf387a3816bf49651a918c9700a72d08
Patrick Macatangga <pmac@Patricks-MBP.fios-router.home>
2017-03-06 20:55:21 -0500

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Created by pmac on 2/18/17.
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+        //int simpleNumber = 5;
+        //String name = "Hello Patrick! ";
+        //System.out.println(name + simpleNumber);
+
+        System.out.println("Hello Patrick!");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        System.out.println("I miss you...");
+    }
+}
